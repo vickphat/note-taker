ad5ac6ba322248618473dbfe3344dc5513250d82
Vicheka Phat <vickphat@gmail.com>
2021-05-09 00:34:48 -0400

    added express code, got page to load properly

--- server.js
+++ server.js
@@ -5,9 +5,37 @@ const path = require("path");
 const data = require("./db.json")
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// Sets up port to deploy on Heroku
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Starts notes.html page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Starts index.html on page load
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Logs that express is listening to port 5000
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App is listening on PORT: " + PORT);
-})
+});
