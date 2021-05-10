c82abafeda77824948851e9065d2d3c1886e6c58
Vicheka Phat <vickphat@gmail.com>
2021-05-09 22:39:43 -0400

    added app.route to obtain db.json data and push it to notes html

--- server.js
+++ server.js
@@ -6,33 +6,27 @@ const data = require("./db.json")
 
 const app = express();
 // Sets up port to deploy on Heroku
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Starts notes.html page
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 
 // Starts index.html on page load
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Starts notes.html page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-
-
-
-
-
-
-
-
-
+// Grabs data from db.json and adds it to notes.html
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(data);
+    })
 
 
 // Logs that express is listening to port 5000
