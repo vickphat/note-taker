9f1f40ef0271acc2137636eca055008e722f177a
Vicheka Phat <vickphat@gmail.com>
2021-05-10 21:28:14 -0400

    modified index.js: removed 'readonly' attribute, user can edit saved notes, fixed new note button

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -54,11 +54,10 @@ const renderActiveNote = () => {
   hide(saveNoteBtn);
 
   if (activeNote.id) {
-    noteTitle.setAttribute('readonly', true);
-    noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.text;
-  } else {
+  } 
+  else {
     noteTitle.value = '';
     noteText.value = '';
   }
